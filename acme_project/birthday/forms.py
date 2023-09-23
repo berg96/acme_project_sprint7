96dd46a3f7c6200c9f8d9b59c3106ae6d34a3d50
Artem Kulikov <chigar2010@yandex.ru>
2023-09-23 13:04:09 +0200

    up

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -6,4 +6,7 @@ class BirthdayForm(forms.Form):
     last_name = forms.CharField(
         label='Фамилия', required=False, help_text='Необязательное поле'
     )
-    birthday = forms.DateField(label='Дата рождения')
+    birthday = forms.DateField(
+        label='Дата рождения',
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
