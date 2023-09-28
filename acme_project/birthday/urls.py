90ba911798c39d05824cf4022ba4af17898c3de5
Artem Kulikov <chigar2010@yandex.ru>
2023-09-28 23:58:20 +0200

    del useless

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -6,7 +6,7 @@ app_name = 'birthday'
 
 urlpatterns = [
     path('', views.BirthdayCreateView.as_view(), name='create'),
-    path('list/', views.birthday_list, name='list'),
+    path('list/', views.BirthdayListView.as_view(), name='list'),
     path('<int:pk>/edit/', views.BirthdayUpdateView.as_view(), name='edit'),
     path(
         '<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete'
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from .forms import BirthdayForm
 from .models import Birthday
-from .utils import calculate_birthday_countdown
+
+
+DATEDIFF = '(strftime("%j","birthday") - strftime("%j","now") + 365) % 365'
 
 
 class BirthdayMixin:
@@ -16,7 +16,10 @@ class BirthdayMixin:
 class BirthdayListView(ListView):
     model = Birthday
     paginate_by = 10
-    ordering = 'id'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.extra(select={'datediff': DATEDIFF}).order_by('datediff')
 
 
 class BirthdayCreateView(BirthdayMixin, CreateView):
@@ -29,43 +32,3 @@ class BirthdayUpdateView(BirthdayMixin, UpdateView):
 
 class BirthdayDeleteView(BirthdayMixin, DeleteView):
     pass
-
-
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-    form = BirthdayForm(
-        request.POST or None, files=request.FILES or None, instance=instance
-    )
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
-
-
-def birthday_list(request):
-    datediff = '(strftime("%j","birthday") - strftime("%j","now") + 365) % 365'
-    birthdays = Birthday.objects.extra(select={'datediff': datediff}).order_by(
-        'datediff'
-    )
-    paginator = Paginator(birthdays, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {'page_obj': page_obj}
-    return render(request, 'birthday/birthday_list.html', context)
-
-
-def delete_birthday(request, pk):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context)
